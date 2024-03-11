301c584a0a3d1c2d715f580a645c004b7f87f230
chaim baror <chbaror@gmail.com>
2024-03-11 23:54:42 +0200

    fix AuthOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,17 @@
-import nextAuth from "next-auth";
+import nextAuth, { AuthOptions } from "next-auth";
 import ProvidersGoogle from "next-auth/providers/google";
 
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
     providers: [
         ProvidersGoogle({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_SECRET!,
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
         }),
     ],
+    session: {
+        strategy: "jwt",
+    },
     secret: process.env.NEXTAUTH_SECRET!,
     pages: {
         signIn: "/signin",
